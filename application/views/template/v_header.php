4b21531cfcf2da32e106150c32dc3d059d2fcb1f
= <imamaulana26.github@gmail.com>
2021-02-03 09:55:25 +0700

    disabled source cdn

--- application/views/template/v_header.php
+++ application/views/template/v_header.php
@@ -29,7 +29,7 @@
 	<!-- Bootstrap Datepicker -->
 	<link rel="stylesheet" href="<?= base_url('assets/') . 'plugins/bootstrap-datepicker/css/bootstrap-datepicker.min.css' ?>">
 	<!-- Google Font: Source Sans Pro -->
-	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+	<!-- <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet"> -->
 
 
 	<!-- REQUIRED SCRIPTS -->
--- application/views/v_login.php
+++ application/views/v_login.php
@@ -13,13 +13,13 @@
 	<!-- Font Awesome -->
 	<link rel="stylesheet" href="<?= base_url('assets/') . 'plugins/fontawesome-free/css/all.min.css' ?>">
 	<!-- Ionicons -->
-	<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+	<!-- <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"> -->
 	<!-- icheck bootstrap -->
 	<link rel="stylesheet" href="<?= base_url('assets/') . 'plugins/icheck-bootstrap/icheck-bootstrap.min.css' ?>">
 	<!-- Theme style -->
 	<link rel="stylesheet" href="<?= base_url('assets/') . 'dist/css/adminlte.min.css' ?>">
 	<!-- Google Font: Source Sans Pro -->
-	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+	<!-- <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet"> -->
 </head>
 
 <body class="hold-transition login-page">
